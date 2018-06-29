7408bb498a3016ddc16918b930c98b43d05d2721
Daniel Malkafly <malkafly@gmail.com>
2018-06-29 06:05:50 -0300

    versao 1.0 no .com.br

---
+++ scripts.js
@@ -0,0 +1,18 @@
+flexFont = function () {
+    var divs = document.getElementsByClassName("flexfont");
+    for (var i = 0; i < divs.length; i++) {
+        var relFontsize = divs[i].offsetWidth * 0.11;
+        divs[i].style.fontSize = relFontsize + 'px';
+    }
+};
+
+window.onload = function (event) {
+    if (window.innerWidth > 1000) {
+        flexFont();
+    }
+};
+window.onresize = function (event) {
+    if (window.innerWidth > 1000) {
+        flexFont();
+    }
+};
