c93fa4685213ac50d929ed201fdd9e77e9732239
D. Malkafly <malkafly@gmail.com>
2019-02-27 17:33:12 -0300

    Bem vindo hiBike!

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 flexFont = function () {
     var divs = document.getElementsByClassName("flexfont");
     for (var i = 0; i < divs.length; i++) {
-        var relFontsize = divs[i].offsetWidth * 0.09;
+        var relFontsize = divs[i].offsetWidth * 0.08;
         divs[i].style.fontSize = relFontsize + 'px';
     }
 };
