03d483346f02a8a57d88b1d281364a7d13b4c4ed
Daniel Malkafly <malkafly@gmail.com>
2023-08-17 09:39:30 -0300

    new portfolio

--- scripts.js
+++ scripts.js
@@ -1,18 +1,1 @@
-flexFont = function () {
-    var divs = document.getElementsByClassName("flexfont");
-    for (var i = 0; i < divs.length; i++) {
-        var relFontsize = divs[i].offsetWidth * 0.08;
-        divs[i].style.fontSize = relFontsize + 'px';
-    }
-};
-
-window.onload = function (event) {
-    if (window.innerWidth > 1000) {
-        flexFont();
-    }
-};
-window.onresize = function (event) {
-    if (window.innerWidth > 1000) {
-        flexFont();
-    }
-};
+//a putaria sempre vence.
